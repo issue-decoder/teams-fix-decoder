8f1f269a5feb841a5ec0454d071b44965efac370
issue-decoder <harishjain@harishjain.com>
2026-05-08 21:42:09 -0400

    Update parser.py
    Update to consume fix tag info from fix_tags lib

--- src/parser.py
+++ src/parser.py
@@ -1,3 +1,5 @@
+from src.fix_tags import FIX_TAGS, SIDE_MAP
+
 def parse_fix(message: str):
     fields = message.split("|")
     result = {}
@@ -5,6 +7,13 @@ def parse_fix(message: str):
     for field in fields:
         if "=" in field:
             tag, value = field.split("=", 1)
-            result[tag] = value
+
+            name = FIX_TAGS.get(tag, tag)
+
+            # decode side
+            if tag == "54":
+                value = SIDE_MAP.get(value, value)
+
+            result[name] = value
 
     return result
