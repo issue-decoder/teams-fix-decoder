e51de81b7c8da3aa15dd7b1ab7529559c83ed9e8
issue-decoder <harishjain@harishjain.com>
2026-05-08 21:39:08 -0400

    Create fix_tags.py

---
+++ src/fix_tags.py
@@ -0,0 +1,16 @@
+FIX_TAGS = {
+    "8": "BeginString",
+    "9": "BodyLength",
+    "35": "MsgType",
+    "49": "SenderCompID",
+    "56": "TargetCompID",
+    "11": "ClOrdID",
+    "55": "Symbol",
+    "54": "Side",
+    "38": "OrderQty"
+}
+
+SIDE_MAP = {
+    "1": "Buy",
+    "2": "Sell"
+}
